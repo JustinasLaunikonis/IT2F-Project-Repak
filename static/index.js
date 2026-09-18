d522157a7bd1dd9dbab1142c2a7ce0ddc3aa4d49
Klaudia <klaudia.zagiel@student.nhlstenden.com>
2026-09-18 13:52:52 +0200

    created a function that shows current activity state depending on which button has been pressed. for now added idle, recording and processing

--- static/index.js
+++ static/index.js
@@ -6,3 +6,30 @@ const testRecordingButton = document.getElementById("test-recording");
 const testProcessingButton = document.getElementById("test-processing");
 const testCompletedButton = document.getElementById("test-completed");
 const testErrorButton = document.getElementById("test-error");
+
+function showState(state) {
+    activityStatus.className = "status status-" + state; //build a class name using selected state, so if 'recording' "status status-" + "recording" and it will use css for this recording state
+
+    if (state === "idle") {
+        activityStatus.textContent = "Activity: Idle";
+        activityMessage.textContent = "Application waiting to start";
+    } else if (state === "recording") {
+        activityStatus.textContent = "Activity: Recording";
+        activityMessage.textContent = "Recording call";
+    } else if (state === "processing") {
+        activityStatus.textContent = "Activity: Processing";
+        activityMessage.textContent = "Processing the call";
+    }
+}
+
+testIdleButton.addEventListener("click", function () {
+    showState("idle");
+});
+
+testRecordingButton.addEventListener("click", function () {
+    showState("recording");
+});
+
+testProcessingButton.addEventListener("click", function () {
+    showState("processing");
+});
