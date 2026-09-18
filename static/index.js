2e10e9520b4bf821eb44da1dcf8caaccad1075d9
Klaudia <klaudia.zagiel@student.nhlstenden.com>
2026-09-18 13:19:49 +0200

    added all constants to index.js

---
+++ static/index.js
@@ -0,0 +1,8 @@
+const activityStatus = document.getElementById("activity-status");
+const activityMessage = document.getElementById("activity-message");
+
+const testIdleButton = document.getElementById("test-idle");
+const testRecordingButton = document.getElementById("test-recording");
+const testProcessingButton = document.getElementById("test-processing");
+const testCompletedButton = document.getElementById("test-completed");
+const testErrorButton = document.getElementById("test-error");
