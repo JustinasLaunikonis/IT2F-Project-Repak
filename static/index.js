e78198d084150611a821cfcd3c2f4672dd283cde
Klaudia <klaudia.zagiel@student.nhlstenden.com>
2026-09-22 13:10:18 +0200

    added more constants to the js, removed disabled from buttons in html bcs now the user needs to be able to begin a real recording

--- static/index.js
+++ static/index.js
@@ -1,11 +1,20 @@
+import { startWavRecording, stopWavRecording } from "./recorder.js"; //these functions come from recorder.js
+
 const activityStatus = document.getElementById("activity-status");
 const activityMessage = document.getElementById("activity-message");
+const connectionStatus = document.getElementById("connection-status");
+
+const startButton = document.getElementById("start-button");
+const stopButton = document.getElementById("stop-button");
 
 const testIdleButton = document.getElementById("test-idle");
 const testRecordingButton = document.getElementById("test-recording");
 const testProcessingButton = document.getElementById("test-processing");
 const testCompletedButton = document.getElementById("test-completed");
 const testErrorButton = document.getElementById("test-error");
+
+let recordingSessionActive = false; //prevents second recording session from being started
+let stopRequested = false; //prevents stop from being requested more than once
 
 function showState(state) {
     activityStatus.className = "status status-" + state; //build a class name using selected state, so if 'recording' "status status-" + "recording" and it will use css for this recording state
